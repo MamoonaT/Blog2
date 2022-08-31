4d47879fec4e4599750f4189ea451a67b67730ac
MamoonaT <mamoonatahir17@gmail.com>
2022-08-31 14:21:58 +0500

    Added Sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: 'Owner', password: '12345'
+	http_basic_authenticate_with name: 'Owner', password: '12345'
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+			user_params.permit(:username, :email)
+		end 
+	end
 end
