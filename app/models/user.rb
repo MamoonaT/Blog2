da66a7ce86728adf1fe34472982feecbbdc42e6c
MamoonaT <mamoonatahir17@gmail.com>
2022-09-01 11:35:59 +0500

    Added functionality for pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def is_traveler?
+    role.name == 'traveler'
+  end
+      
+  def is_travel_agent?
+     role.name == 'travel_agent'
+  end
 end
